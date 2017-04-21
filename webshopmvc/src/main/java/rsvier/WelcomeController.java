e3ec0209926479e1fbb487f66e1c66080fe0d0c6
Rutger de Kok <de.kok.rutger@gmail.com>
2017-04-21 10:50:42 +0200

    Table example from skype meeting with Remi

--- webshopmvc/src/main/java/rsvier/WelcomeController.java
+++ webshopmvc/src/main/java/rsvier/WelcomeController.java
@@ -1,22 +1,42 @@
 package rsvier;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
+
+import rsvier.product.Product;
+import rsvier.product.ProductCategory;
 
 @Controller
 class WelcomeController {
 
+	// @RequestMapping("/")
+	// public String welcome() {
+	// return "welcome";
+	// }
+
 	@RequestMapping("/")
-	public String welcome() {
+	public String welcome(Map<String, Object> model) {
+
+		Product product1 = new Product();
+		product1.setName("kaas");
+		product1.setCategory(ProductCategory.GOAT);
+		product1.setPrice(new BigDecimal("12.30"));
+
+		List<String> list = Arrays.asList(new String[] { "een", "twee", "drie", "vier", "vijf" });
+		List<Product> products = new ArrayList<>();
+		products.add(product1);
+		products.add(product1);
+		products.add(product1);
+		products.add(product1);
+
+		model.put("lijst", products);
 		return "welcome";
 	}
 
-	//
-	// @RequestMapping("/")
-	// public RedirectView redirect() {
-	//
-	// return new RedirectView("/welcome.html");
-	// }
 }
