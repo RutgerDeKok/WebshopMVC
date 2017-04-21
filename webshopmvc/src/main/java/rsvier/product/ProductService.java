c7f46612c77d10e009b23cedade7e88083ad5947
Tjeerd <tjeerdsantema@hotmail.com>
2017-04-21 17:39:12 +0200

    welkompagina bevat nu kazen die in Service worden aangemaakt

--- webshopmvc/src/main/java/rsvier/product/ProductService.java
+++ webshopmvc/src/main/java/rsvier/product/ProductService.java
@@ -10,12 +10,21 @@ import org.springframework.stereotype.Component;
 @Service
 public class ProductService {
     
+    
+    Product product = new Product(1, "Groene kaas", "Groen", ProductCategory.CREAM, "informatie", (new BigDecimal("2.50")), 200);
+    Product product1 = new Product(2, "Gele kaas", "geel", ProductCategory.GOAT, "info", (new BigDecimal("4.99")), 200);
+    Product product2 = new Product(3, "Kaas de la kaas", "uberkaas", ProductCategory.MEDIUM_HARD, "info", (new BigDecimal("1.99")), 50);
+    
+    
     @Autowired
     ProductRepository dao;
     
      public List<Product> getAllProducts(){
      List<Product> list = new ArrayList<>();
      dao.findAll().forEach(list::add);
+     list.add(product);
+     list.add(product1);
+     list.add(product2);
      return list;
   }
     
