faeafcf49459aa421f0583e4f666262cd9d831f4
Rutger de Kok <de.kok.rutger@gmail.com>
2017-04-24 13:42:05 +0200

    Login controller takes post request and sets current user

--- webshopmvc/src/main/java/rsvier/LoginController.java
+++ webshopmvc/src/main/java/rsvier/LoginController.java
@@ -1,13 +1,16 @@
 package rsvier;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import rsvier.product.Product;
-import rsvier.product.ProductCategory;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import rsvier.infrastructure.PassHasher;
+import rsvier.infrastructure.Validator;
 import rsvier.user.User;
 import rsvier.user.UserService;
 
@@ -19,36 +22,78 @@ import rsvier.user.UserService;
 @Controller
 public class LoginController {
 
-
+	@Autowired
+	private UserService userService;
+	
+	private User currentUser;
 
 	// @RequestMapping("/")
 	// public String welcome() {
 	// return "welcome";
 	// }
-    
-    private final UserService UserService;
+
+	@RequestMapping(value = "login", method = RequestMethod.POST)
+	public @ResponseBody void filterProducts(HttpServletRequest request, HttpServletResponse response) {
+		String uname = request.getParameter("uname");
+		String pass = request.getParameter("psw");
+
+		char[] passChars = pass.toCharArray();
+
+		User user = null;
+
+		// get user by email
+		try {
+
+			Validator validator = Validator.getInstance();
+			if (validator.validateEmail(uname)) {
+				user = userService.findUserByEmail(uname);
+			} else {
+				System.out.println("Not a valid user");
+			}
+
+			// check password
+			if (PassHasher.check(passChars, user.getPassHash())) {
+				System.out.println("login succesful!");
+				// current user set
+				currentUser = user;
+			} else {
+				System.out.println("login incorrect!");
+			}
+			
+			System.out.println("user email" + user.getEmail());
+			System.out.println("user id" + user.getId());
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		
+		}
+
+	
+
+		//
+		// try {
+		// response.sendRedirect("/products");
+		// } catch (IOException e) {
+		//
+		// e.printStackTrace();
+		// }
+	}
+
+	private final UserService UserService;
 
 	@Autowired
 	public LoginController(UserService iets) {
 		this.UserService = iets;
 	}
-    
-    
 
 	@RequestMapping(value = { "/login.html" })
-        public String inlog(){
-        return "login";
-        }
-        
-
-		
-		
+	public String inlog() {
+		return "login";
 	}
-        
-        
-        
+	
+	public User getCurrentUser() {
+		return currentUser;
+	}
 
 
-    
-    
-
+}
--- webshopmvc/src/main/java/rsvier/user/UserRepository.java
+++ webshopmvc/src/main/java/rsvier/user/UserRepository.java
@@ -3,5 +3,8 @@ package rsvier.user;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long>{
+	
+	public User findUserByEmail(String email);
+
 
 }
