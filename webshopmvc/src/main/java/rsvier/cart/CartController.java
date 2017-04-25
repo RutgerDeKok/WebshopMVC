d2ef1b15a8ecba1bfe9dd74aedd6ec6ca8d08246
J-PC\J <jurjen.s@gmail.com>
2017-04-25 16:15:11 +0200

    Suborder uit cart verwijderen werkt, maar moet wel cart in db hebben

--- webshopmvc/src/main/java/rsvier/cart/CartController.java
+++ webshopmvc/src/main/java/rsvier/cart/CartController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import rsvier.cartsuborder.CartSubOrder;
+import rsvier.cartsuborder.CartSubOrderService;
 import rsvier.product.Product;
 import rsvier.product.ProductCategory;
 
@@ -25,38 +26,26 @@ import rsvier.user.User;
 public class CartController {
     
     @Autowired
+    ProductService productService;
+    @Autowired
+    CartSubOrderService cartSubOrderService;
+    @Autowired
     CartService cartService;
     Cart cart = new Cart();
-    
-    
+
     @RequestMapping("/cart")
-    public String shoppingCart(Model model) {
-        Product product = new Product(1, "kaas", "merk", ProductCategory.GOAT, "info", (new BigDecimal("2.50")), 200);
-        CartSubOrder cso = new CartSubOrder(1, product, 5);
-        cso.setSubTotal(product.getPrice(), cso.getQuantity());
-        CartSubOrder cso2 = new CartSubOrder(2, product, 3);
-        cso2.setSubTotal(product.getPrice(), cso2.getQuantity());
-        cart.addSubOrder(cso);
-        cart.addSubOrder(cso2);
-        cart.calculateTotalPrice();
+    public Cart shoppingCart(Model model) {
+        cartService.getCart(1L);
         model.addAttribute("cart", cart);
         return "cart";
     }
 
-    @RequestMapping(value="/cart/{id}")
-    public String deleteProduct(Model model, @PathVariable String id){
-       long idL = Long.parseLong(id);
-       
-        cart.getSubOrders().remove(id);
-        
-        model.addAttribute("cart", cart);
-        return "cart";
-        /* Zo gaat het ongeveer moeten met een DB
-        List<CartSubOrder> list = new ArrayList<>();
-        list = cartService.getCart(1L).getSubOrders();
-        list.remove(id);
-        cartService.deleteCart(cart);
-        */
+    @RequestMapping(value="/cart/delete", method = RequestMethod.GET)
+    public String removeFromCart(@RequestParam long cartId, @RequestParam long subOrderId) {
+        Cart cart = cartService.getCart(cartId);
+        CartSubOrder cso = cartSubOrderService.getCartSubOrder(subOrderId);
+        cartService.removeFromCart(cart, cso);
+        return "redirect:/cart";
     }
      @RequestMapping("/checkout")
     public String checkout(Model model) {
--- webshopmvc/src/main/java/rsvier/cart/CartService.java
+++ webshopmvc/src/main/java/rsvier/cart/CartService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.stereotype.Component;
+import rsvier.cartsuborder.CartSubOrder;
 
 @Service
 public class CartService {
@@ -13,27 +14,33 @@ public class CartService {
     @Autowired
     CartRepository dao;
     
-     public List<Cart> getAllCarts(){
-     List<Cart> list = new ArrayList<>();
-     dao.findAll().forEach(list::add);
-     return list;
-  }
+    public List<Cart> getAllCarts(){
+         List<Cart> list = new ArrayList<>();
+         dao.findAll().forEach(list::add);
+         return list;
+    }
     
     public Cart getCart(Long id){
         return dao.findOne(id);
-    
     }
     
-    public void updateCart(Long id, Cart cart){
+    public void updateCart(Cart cart){
        dao.save(cart);
-        
     }
     
-     public void deleteCart(Cart cart){
+    public void deleteCart(Cart cart){
          dao.delete(cart);
     }
-     
-      public void createCart(Cart cart){
+
+    public void createCart(Cart cart){
          dao.save(cart);
     }
+
+    public void removeFromCart(Cart cart, CartSubOrder cso) {
+        List<CartSubOrder> updatedList = cart.getSubOrders();
+        updatedList.remove(cso);
+        cart.setSubOrders(updatedList);
+        updateCart(cart);
+    }
+
 }
