3257063d9ca2018debf12863e8c272d8a6b9acc4
Tjeerd <tjeerdsantema@hotmail.com>
2017-04-25 13:31:03 +0200

    1)AfrekenPagina toegevoegd met lege links naar 'wijzigAdres' en 'Afrekenen' 2)Winkelwagen en afrekenpagina linken met elkaar
    De userAddress is hardcoded in CartController

--- webshopmvc/src/main/java/rsvier/cart/CartController.java
+++ webshopmvc/src/main/java/rsvier/cart/CartController.java
@@ -11,9 +11,14 @@ import rsvier.product.ProductCategory;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import rsvier.address.Address;
+import rsvier.user.User;
 
 
 @Controller
@@ -25,7 +30,7 @@ public class CartController {
     
     
     @RequestMapping("/cart")
-    public Cart shoppingCart(Model model) {
+    public String shoppingCart(Model model) {
         Product product = new Product(1, "kaas", "merk", ProductCategory.GOAT, "info", (new BigDecimal("2.50")), 200);
         CartSubOrder cso = new CartSubOrder(1, product, 5);
         cso.setSubTotal(product.getPrice(), cso.getQuantity());
@@ -35,15 +40,17 @@ public class CartController {
         cart.addSubOrder(cso2);
         cart.calculateTotalPrice();
         model.addAttribute("cart", cart);
-        return cart;
+        return "cart";
     }
 
-    @RequestMapping(method= RequestMethod.DELETE, value="/cart/{id}")
-    public void deleteProduct(@PathVariable String id){
+    @RequestMapping(value="/cart/{id}")
+    public String deleteProduct(Model model, @PathVariable String id){
        long idL = Long.parseLong(id);
        
         cart.getSubOrders().remove(id);
         
+        model.addAttribute("cart", cart);
+        return "cart";
         /* Zo gaat het ongeveer moeten met een DB
         List<CartSubOrder> list = new ArrayList<>();
         list = cartService.getCart(1L).getSubOrders();
@@ -51,5 +58,23 @@ public class CartController {
         cartService.deleteCart(cart);
         */
     }
-     
+     @RequestMapping("/checkout")
+    public String checkout(Model model) {
+       Product product = new Product(1, "kaas", "merk", ProductCategory.GOAT, "info", (new BigDecimal("2.50")), 200);
+        CartSubOrder cso = new CartSubOrder(1, product, 5);
+        cso.setSubTotal(product.getPrice(), cso.getQuantity());
+        CartSubOrder cso2 = new CartSubOrder(2, product, 3);
+        cso2.setSubTotal(product.getPrice(), cso2.getQuantity());
+        cart.addSubOrder(cso);
+        cart.addSubOrder(cso2);
+        cart.calculateTotalPrice();
+        model.addAttribute("cart", cart);
+        Address address = new Address(2, "Tjeerd", "van", "Santema", "hiero", 22, "b", "9283 AG", "Groningen");
+        User user = new User();
+        user.setBillingAdress(address);
+        model.addAttribute("user", user);
+        
+        
+        return "Checkout";
+    }
 }
