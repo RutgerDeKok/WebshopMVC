ddaa210e1f9f6811f2e006c92d217b320dd3b0a3
FRANK <lorus1@live.nl>
2017-04-26 13:08:18 +0200

    basis opzet

---
+++ webshopmvc/src/main/java/rsvier/RegisteerController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rsvier;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import rsvier.infrastructure.PassHasher;
+import rsvier.infrastructure.Validator;
+import rsvier.user.User;
+import rsvier.user.UserService;
+/**
+ *
+ * @author Frank
+ */
+
+@Controller
+public class RegisteerController {
+ 
+    
+    
+    
+    
+    
+    
+    @RequestMapping(value = { "/registeren" })
+	public String inlog() {
+		return "registeren";
+	}
+    
+    
+    
+}
