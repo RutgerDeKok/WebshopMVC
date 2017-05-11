d62a39e150eb629e05dfbeb0f3cc9dd4a1f4743d
Nadkine <tjeerdsantema@hotmaill.com>
2017-05-11 12:23:32 +0200

    ErrorPage toegevoegd

--- webshopmvc/src/main/resources/import.sql
+++ webshopmvc/src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO products VALUES (DEFAULT, "Danablue", "BLUE", "info", "Danablue", "2.50", "500");
 INSERT INTO products VALUES (DEFAULT, "President", "GOAT", "info", "Geitenkaas", "2.50", "500");
 INSERT INTO products VALUES (DEFAULT, "Paturain", "CREAM", "info", "Kruidenroomkaas", "2.50", "500");
+INSERT INTO users VALUES (3, "baas@baas.nl", TRUE, "$2a$10$8MED3xmhrqsnvr5gMn2gXOGVENqXpemDeMsspCS8.lwI0rp/UU09q",  "EMPLOYEE", 1);
