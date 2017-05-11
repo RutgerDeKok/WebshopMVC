9824d1fd784166b6d3d7411662647800d3761320
Nadkine <tjeerdsantema@hotmaill.com>
2017-05-11 12:22:48 +0200

    ErrorPage toegvoegd

--- webshopmvc/src/main/java/rsvier/WebshopApplication.java
+++ webshopmvc/src/main/java/rsvier/WebshopApplication.java
@@ -1,7 +1,9 @@
 package rsvier;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
 public class WebshopApplication {
